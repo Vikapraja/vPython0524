7e13dc9aa5edc2196280b9fbae15d73c32ce728d
Vivekanand <109669780+Vikapraja@users.noreply.github.com>
2024-05-06 11:49:41 +0530

    conditional statements
    conditional statements

--- p37.py
+++ p37.py
@@ -1,3 +1,14 @@
+#Conditional Statements
+    #are those statements that are only executed if a given condition is 
+    #True or False
+
+#if,else,elif keywords are used to implement the concept of conditional stmts
+#syntax-
+#if boolean-expression:
+#   stmts
+#else:
+#   stmts2
+
 marks=float(input('Enter marks='))
 if marks>=40:
     print('pass')
