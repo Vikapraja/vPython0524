a2e7e9bd58c5d5791fc2dca8cd585036c32a520f
Vivekanand <109669780+Vikapraja@users.noreply.github.com>
2024-05-06 12:42:15 +0530

    Add files via upload

--- dollar_to_inr.py
+++ dollar_to_inr.py
@@ -1,3 +1,9 @@
-d=float(input('Enter dollar='))
-inr=d*83.51
-print(round(inr,2))
+#dollar to inr
+#d=float(input('Enter dollar='))
+#inr=d*83.51
+#print(round(inr,2))
+
+v=float(input("Enter dollar's vallue="))
+n=int(input("Enter dollar's no.="))
+inr=v*n
+print('inr=',inr)
---
+++ even_odd.py
@@ -0,0 +1,6 @@
+#wap to check even/odd of number
+a=float(input('enter a number to check even or odd='))
+if a%2==0:
+    print('Even')
+else:
+    print('Odd')
