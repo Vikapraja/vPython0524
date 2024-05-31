90d11efaedcb73376519e35830548711031c36d7
Vivekanand <vibekapotter0@gmail.com>
2024-05-31 11:13:38 +0530

    Library-gmail,pytube, FILE-I/O

--- p113.py
+++ p113.py
@@ -1,11 +1,14 @@
-#'w' and 'r' mode
+#'w' mode 
+    #for write in the file
+    #it create file if not found
 
 file=open("abc.txt","w")
 file.write("jay ho\n")
 file.write("hi")
 file.close()
 
-
+#'r' mode
+    #for read() only
 file=open("abc.txt","r")
 a=file.read()
 file.close()
